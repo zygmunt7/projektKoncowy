cfc0b09b149d9cf4cb68b7c3cf3b06f362ee36e3
zygmunt7 <zygmunt.jasinski@pollub.edu.pl>
2022-10-22 10:50:37 +0200

    init project

---
+++ src/main/java/pl/sdacademy/ConferenceRoomReservationSystem/ConferenceRoomReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.ConferenceRoomReservationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceRoomReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceRoomReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sdacademy/ConferenceRoomReservationSystem/ConferenceRoomReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.ConferenceRoomReservationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceRoomReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
